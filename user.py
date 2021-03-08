d9d5b498baa2b701d437b3d1ff8851e9a1e25662
Haoran Lyu <haoran.lyu@student.unsw.edu.au>
2021-03-08 11:50:50 +0800

    added function header and description to login.py and database.py

---
+++ user.py
@@ -0,0 +1,18 @@
+'''
+    This file contains user side functions
+'''
+
+def edit_info_user(id):
+    '''
+        This function edits user info
+    '''
+    return {
+    }
+
+def add_fund_user(id, fund):
+    '''
+        This function adds fund to a user
+    '''
+    return {
+        "current_fund": fund
+    }
