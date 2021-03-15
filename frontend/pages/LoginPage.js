1ad1b83419f08e2db422a469de6a2806e4ab8c7d
Pai Qu <pai.qu@outlook.com>
2021-03-16 00:25:41 +1100

    LoginPage component done

---
+++ frontend/pages/LoginPage.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function LoginPage() {
+    return (
+        <div>
+            <div className="sidenav">
+                <div className="login-main-text">
+                    <h1><span id="login-yellow">Log in</span> to your<br /> V-Gift account</h1>
+                </div>
+            </div>
+            <main className="main">
+                <div className="col-md-6 col-sm-12">
+                    <form className="login-form">
+                        <div className="form-group">
+                            <label>User Name</label>
+                            <input id="login_user_name" type="text" className="form-control" placeholder="User Name" />
+                        </div>
+                        <div className="form-group">
+                            <label>Password</label>
+                            <input id="login_user_password" type="password" className="form-control" placeholder="Password" />
+                        </div>
+                        <button type="button" className="btn btn-primary btn-lg btn-block">Log in</button>
+                        <a role="button" className="btn btn-secondary btn-lg btn-block"  href="register.html">Not a member? Sign up</a>
+                    </form>
+                </div>
+            </main>
+        </div>
+    );
+}
+
+export default LoginPage;
