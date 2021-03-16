371aeb4e13c29923f5fa92534be6148ec7c7a48b
Pai Qu <pai.qu@outlook.com>
2021-03-16 20:29:24 +1100

    convert to react DONE

--- frontend/src/pages/ProductsPage.js
+++ frontend/src/pages/ProductsPage.js
@@ -9,7 +9,7 @@ function ProductsPage(props) {
             <main className="d-flex justify-content-center align-items-center flex-column">
                 <div className="products-grid container-fluid w-75 d-grid gap-2">
                     <div className="row">
-                        <div className="col d-flex flex-column align-items-center me-2">
+                        <div className="product-info col d-flex flex-column align-items-center me-2">
                             <img className="mt-1" src="img/products/product_1.jpeg" alt="" />
                             <p className="fs-3">Mario 1</p>
                             <p className="fs-4">$299</p>
@@ -17,7 +17,7 @@ function ProductsPage(props) {
                                 <i className="bi bi-cart-plus add-cart-icon"></i>
                             </button>
                         </div>
-                        <div className="col d-flex flex-column align-items-center me-2">
+                        <div className="product-info col d-flex flex-column align-items-center me-2">
                             <img className="mt-1" src="img/products/product_2.jpeg" alt="" />
                             <p className="fs-3">Mario 2</p>
                             <p className="fs-4">$299</p>
@@ -25,7 +25,7 @@ function ProductsPage(props) {
                                 <i className="bi bi-cart-plus add-cart-icon"></i>
                             </button>
                         </div>
-                        <div className="col d-flex flex-column align-items-center me-2">
+                        <div className="product-info col d-flex flex-column align-items-center me-2">
                             <img className="mt-1" src="img/products/product_3.jpeg" alt="" />
                             <p className="fs-3">Mario 3</p>
                             <p className="fs-4">$299</p>
@@ -35,7 +35,7 @@ function ProductsPage(props) {
                         </div>
                     </div>
                     <div className="row">
-                        <div className="col d-flex flex-column align-items-center me-2">
+                        <div className="product-info col d-flex flex-column align-items-center me-2">
                             <img className="mt-1" src="img/products/product_1.jpeg" alt="" />
                             <p className="fs-3">Mario 1</p>
                             <p className="fs-4">$299</p>
@@ -43,7 +43,7 @@ function ProductsPage(props) {
                                 <i className="bi bi-cart-plus add-cart-icon"></i>
                             </button>
                         </div>
-                        <div className="col d-flex flex-column align-items-center me-2">
+                        <div className="product-info col d-flex flex-column align-items-center me-2">
                             <img className="mt-1" src="img/products/product_2.jpeg" alt="" />
                             <p className="fs-3">Mario 2</p>
                             <p className="fs-4">$299</p>
@@ -51,7 +51,7 @@ function ProductsPage(props) {
                                 <i className="bi bi-cart-plus add-cart-icon"></i>
                             </button>
                         </div>
-                        <div className="col d-flex flex-column align-items-center me-2">
+                        <div className="product-info col d-flex flex-column align-items-center me-2">
                             <img className="mt-1" src="img/products/product_3.jpeg" alt="" />
                             <p className="fs-3">Mario 3</p>
                             <p className="fs-4">$299</p>
@@ -61,7 +61,7 @@ function ProductsPage(props) {
                         </div>
                     </div>
                 </div>
-                <div className="container-fluid w-75">
+                <div className="container-fluid w-75 d-flex flex-column align-items-center">
                     <p className="mt-3">Page 1 of 15</p>
                     <div className="d-flex mb-3 btn-group">
                         <button className="btn btn-lg btn-secondary" href="#">Last Page</button>
