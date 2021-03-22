9b6c8b3e2feb0183ccdf0212ac19aa73b4330fce
Pai Qu <pai.qu@outlook.com>
2021-03-22 23:17:02 +1100

    admin page users info done

---
+++ frontend/src/components/AdminDrawer.js
@@ -0,0 +1,236 @@
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+import NavBar from './NavBar';
+import { Link } from 'react-router-dom';
+import Avatar from '@material-ui/core/Avatar';
+import Box from '@material-ui/core/Box';
+import GroupIcon from '@material-ui/icons/Group';
+import AttachMoneyIcon from '@material-ui/icons/AttachMoney';
+import StoreIcon from '@material-ui/icons/Store';
+import UsersDataGrid from './UsersDataGrid';
+import HomeIcon from '@material-ui/icons/Home';
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: 36,
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+  },
+  drawerOpen: {
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerClose: {
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: 'hidden',
+    width: theme.spacing(7) + 1,
+    [theme.breakpoints.up('sm')]: {
+      width: theme.spacing(9) + 1,
+    },
+  },
+  toolbar: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+  logo: {
+    margin: "auto",
+    textAlign: 'center',
+    maxWidth: "auto",
+    maxHeight: '7vh',
+    marginRight: "0.5rem",
+  },
+}));
+
+
+
+export default function MiniDrawer() {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const renderUsers = (
+    <UsersDataGrid />
+  );
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+        style={{
+          boxShadow: 'none',
+        }}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, {
+              [classes.hide]: open,
+            })}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+						style={{
+							textDecoration: "none",
+              fontWeight: "200",
+              color: theme.palette.primary.contrastText
+						}}
+						color="inherit"
+						component={Link}
+						to={'/'}
+						className={classes.title} 
+						variant="h4"
+						noWrap
+					>
+            V-Gifts | Admin Page
+          </Typography>
+
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant="permanent"
+        className={clsx(classes.drawer, {
+          [classes.drawerOpen]: open,
+          [classes.drawerClose]: !open,
+        })}
+        classes={{
+          paper: clsx({
+            [classes.drawerOpen]: open,
+            [classes.drawerClose]: !open,
+          }),
+        }}
+      >
+        <div className={classes.toolbar}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          <ListItem button key={"Home"}>
+            <ListItemIcon><HomeIcon /></ListItemIcon>
+            <ListItemText primary={"Home"} />
+          </ListItem>
+          <ListItem button key={"Inbox"}>
+            <ListItemIcon><InboxIcon /></ListItemIcon>
+            <ListItemText primary={"Inbox"} />
+          </ListItem>
+          <ListItem button key={"Market"}>
+            <ListItemIcon><StoreIcon /></ListItemIcon>
+            <ListItemText primary={"Market"} />
+          </ListItem>
+          <ListItem button key={"Users"}>
+            <ListItemIcon><GroupIcon /></ListItemIcon>
+            <ListItemText primary={"Users"} />
+          </ListItem>
+          <ListItem button key={"Orders"}>
+            <ListItemIcon><AttachMoneyIcon /></ListItemIcon>
+            <ListItemText primary={"Orders"} />
+          </ListItem>
+        </List>
+        <Divider />
+        {/* <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List> */}
+      </Drawer>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Box
+          display="flex" 
+          flexDirection="column"
+          alignItems="center"
+        >
+          <Avatar 
+            alt="Admin Avartar"
+            src="/img/admin/admin-1.png"
+            variant="rounded" 
+            style={{
+              border: `1px solid ${theme.palette.primary.contrastText}`,
+              width: theme.spacing(20),
+              height: theme.spacing(20),
+              marginBottom: theme.spacing(5),
+            }}
+          />
+        </Box>
+        {renderUsers}
+      </main>
+    </div>
+  );
+}
