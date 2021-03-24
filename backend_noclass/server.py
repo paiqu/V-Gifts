a4204af5c0d9ced0927b8a514f3a23dd2ad48691
Pai Qu <pai.qu@outlook.com>
2021-03-24 18:34:54 +1100

    add init for server

--- backend_noclass/server.py
+++ backend_noclass/server.py
@@ -276,3 +276,7 @@ def refund_order():
     return dumps({
         'status': result
     })
+    
+if __name__ == "__main__":
+    
+    app.run(port=(int(sys.argv[1]) if len(sys.argv) == 2 else 8080))
