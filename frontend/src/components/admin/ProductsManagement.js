b4932fba384c1680761b1d308396de70ceaf4ba2
Pai Qu <pai.qu@outlook.com>
2021-04-14 00:01:24 +1000

    add editted product display in admin's page

--- frontend/src/components/admin/ProductsManagement.js
+++ frontend/src/components/admin/ProductsManagement.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from "@material-ui/core/Typography";
 import { DataGrid } from '@material-ui/data-grid';
@@ -7,7 +7,8 @@ import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import axios from 'axios';
 import { Grid } from '@material-ui/core';
-
+import ImageIcon from '@material-ui/icons/Image';
+import DoneIcon from '@material-ui/icons/Done';
 
 const useStyles = makeStyles((theme) => ({
   form: {
@@ -23,8 +24,18 @@ export default function ProductsManagement(props) {
     price: 0,
     description: "",
     delivery: "",
-    img: "",
+    img: null,
   });
+  const [selectedProduct, setSelectedProduct] = useState({
+    name: "",
+    price: 0,
+    description: "",
+    delivery: "",
+    img: null,
+  });
+
+  const [isUploaded, setIsUploaded] = useState(false);
+  const [selectionModel, setSelectionModel] = useState([]);
 
   const columns = [
     { field: 'id', headerName: 'Product ID', width: 150},
@@ -52,8 +63,41 @@ export default function ProductsManagement(props) {
 
   const handleAddProduct = (event) => {
     event.preventDefault();
+
+    let imgData = new FormData();
+    imgData.append("img", newProduct.img)
   };
 
+  const handleFileUpload = (event) => {
+    setNewProduct({
+      ...newProduct,
+      img: event.target.files[0],
+    });
+    setIsUploaded(true);
+  }
+
+  useEffect((() => {
+    if (selectionModel.length != 0) {    
+      axios.get('/product/get_info', {
+          params: {
+            id: parseInt(selectionModel),
+          }
+        })
+      .then((response) => {
+        const data = response.data;
+        
+        setSelectedProduct({
+          name: data['name'],
+          price: data['price'],
+          description: data['description'],
+          // delivery: data['delivery'],
+          // rating: data['rating'],
+          // img: data['pic_link'],
+        });
+      })
+      .catch((err) => {});
+    }
+  }), [selectionModel]);
 
   return (
     <div style={{width: '100%'}}>
@@ -64,93 +108,301 @@ export default function ProductsManagement(props) {
         pageSize={5} 
         checkboxSelection 
         autoHeight
-      />
-      <form className={classes.form} onSubmit={handleAddProduct}>
-        <Typography variant="h5" style={{marginBottom: "1rem"}}>
-          Add a New Product
-        </Typography>
-        <Grid
-          container
-          spacing={2}
-        >
-          <Grid 
-            container 
-            item 
-            xs={12} 
-            spacing={2}
-          >
-            <Grid item xs={2}>
-              <TextField
-                required
-                id="product-name"
-                label="Product Name"
-                placeholder="Admin Name"
-                variant="outlined"
-                onChange={handleChange('name')}
-                style={{
-                  marginRight: "1rem",
-                  width: "100%",
-                }}
-              />
-            </Grid>
-            <Grid item xs={2}>
-              <TextField
-                required
-                id="product-price"
-                label="Price"
-                placeholder="Product Price"
-                type="number"
-                variant="outlined"
-                onChange={handleChange('price')}
-                inputProps={{
-                  step: 10,
-                  min: 0,
-                }}
-                value={newProduct.price}
-                style={{
-                  width: "100%",
-                }}
-              />
-            </Grid>
-          </Grid>
-          <Grid
-            container
-            item
-            xs={12}
-            spacing={2}
-          >
-            <Grid item xs={4}>
-              <TextField
-                required
-                id="product-description"
-                label="Description"
-                placeholder="Product Description"
-                variant="outlined"
-                onChange={handleChange('description')}
-                style={{
-                  marginRight: "1rem",
-                  width: "100%",
-                }}
-                multiline
-                rows={4}
-                rowsMax={4}
-              />
-            </Grid>
-          </Grid>
+        selectionModel={selectionModel}
+        hideFooterSelectedRowCount
+        // disableMultipleSelection={true}
+        onSelectionModelChange={(selection) => {
+          const newSelectionModel = selection.selectionModel;
 
-          <Button 
-            type="submit"
-            variant="contained" 
-            color="primary"
-            style={{
-              marginLeft: "1rem",
-              height: "100%",
-            }}
-          >
-            Add
-          </Button>
+          if (newSelectionModel.length > 1) {
+            const selectionSet = new Set(selectionModel);
+            const result = newSelectionModel.filter(
+              (s) => !selectionSet.has(s)
+            );
+
+            setSelectionModel(result);
+          } else {
+            setSelectionModel(newSelectionModel);
+          }
+        }}
+      />
+      <Grid container>
+        <Grid container item xs={6}>
+          <form className={classes.form} onSubmit={handleAddProduct}>
+            <Grid
+              container
+              spacing={2}
+              xs={12}
+            >
+              <Grid item xs={12}>
+                <Typography variant="h5" style={{marginBottom: "1rem"}}>
+                  Add a New Product
+                </Typography>
+              </Grid>
+              <Grid
+                container
+                item
+                xs={12}
+                spacing={2}
+              >
+                <Grid item xs={5}>
+                  <TextField
+                    required
+                    id="product-name"
+                    label="Product Name"
+                    placeholder="Admin Name"
+                    variant="outlined"
+                    onChange={handleChange('name')}
+                    style={{
+                      marginRight: "1rem",
+                      width: "100%",
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={5}>
+                  <TextField
+                    required
+                    id="product-price"
+                    label="Price"
+                    placeholder="Product Price"
+                    type="number"
+                    variant="outlined"
+                    onChange={handleChange('price')}
+                    inputProps={{
+                      step: 10,
+                      min: 0,
+                    }}
+                    value={newProduct.price}
+                    style={{
+                      width: "100%",
+                    }}
+                  />
+                </Grid>
+              </Grid>
+              <Grid
+                container
+                item
+                xs={12}
+                spacing={2}
+              >
+                <Grid item xs={10}>
+                  <TextField
+                    required
+                    id="product-description"
+                    label="Description"
+                    placeholder="Product Description"
+                    variant="outlined"
+                    onChange={handleChange('description')}
+                    style={{
+                      marginRight: "1rem",
+                      width: "100%",
+                    }}
+                    multiline
+                    rows={4}
+                    rowsMax={4}
+                  />
+                </Grid>
+              </Grid>
+              <Grid
+                container
+                item
+                xs={12}
+                spacing={2}
+              >
+                <Grid item xs={5}>
+                  { !isUploaded &&
+                    <Button
+                      variant="contained"
+                      component="label"
+                      color="primary"
+                      style={{
+                        width: "100%"
+                      }}
+                    >
+                      <ImageIcon />Upload Image
+                      <input
+                        type="file"
+                        accept="image/*"
+                        hidden
+                        onChange={handleFileUpload}
+                      />
+                    </Button>
+                  }
+                  { isUploaded &&
+                    <Button
+                      variant="contained"
+                      component="label"
+                      color="primary"
+                      style={{
+                        width: "100%"
+                      }}
+                    >
+                      <DoneIcon />Image Uploaded
+                      <input
+                        type="file"
+                        accept="image/*"
+                        hidden
+                        onChange={handleFileUpload}
+                      />
+                    </Button>
+                  }
+                </Grid>
+                <Grid item xs={5}>
+                  <Button
+                    type="submit"
+                    variant="contained"
+                    color="secondary"
+                    style={{
+                      width: "100%",
+                    }}
+                  >
+                    Add New Product
+                  </Button>
+                </Grid>
+              </Grid>
+            </Grid>
+          </form>
         </Grid>
-      </form>
+        <Grid container item xs={6}>
+          <form className={classes.form} onSubmit={handleAddProduct}>
+            <Grid
+              container
+              spacing={2}
+            >
+            <Grid item xs={12}>
+              <Typography variant="h5" style={{marginBottom: "1rem"}}>
+                Edit an Existed Product (Current Selected: id-{selectionModel})
+              </Typography>
+            </Grid>
+              <Grid
+                container
+                item
+                xs={12}
+                spacing={2}
+              >
+                <Grid item xs={5}>
+                  <TextField
+                    required
+                    id="product-name"
+                    label="Name"
+                    placeholder="Product Name"
+                    variant="outlined"
+                    onChange={handleChange('name')}
+                    InputLabelProps={{shrink: true}}
+                    style={{
+                      marginRight: "1rem",
+                      width: "100%",
+                    }}
+                    value={selectedProduct.name}
+                  />
+                </Grid>
+                <Grid item xs={5}>
+                  <TextField
+                    required
+                    id="product-price"
+                    label="Price"
+                    // placeholder="Product Price"
+                    type="number"
+                    variant="outlined"
+                    onChange={handleChange('price')}
+                    inputProps={{
+                      step: 10,
+                      min: 0,
+                    }}
+                    value={newProduct.price}
+                    style={{
+                      width: "100%",
+                    }}
+                    value={selectedProduct.price}
+                  />
+                </Grid>
+              </Grid>
+              <Grid
+                container
+                item
+                xs={12}
+                spacing={2}
+              >
+                <Grid item xs={10}>
+                  <TextField
+                    required
+                    id="product-description"
+                    label="Description"
+                    // placeholder="Product Description"
+                    variant="outlined"
+                    onChange={handleChange('description')}
+                    style={{
+                      marginRight: "1rem",
+                      width: "100%",
+                    }}
+                    multiline
+                    rows={4}
+                    rowsMax={4}
+                    value={selectedProduct.description}
+                  />
+                </Grid>
+              </Grid>
+              <Grid
+                container
+                item
+                xs={12}
+                spacing={2}
+              >
+                <Grid item xs={5}>
+                  { !isUploaded &&
+                    <Button
+                      variant="contained"
+                      component="label"
+                      color="primary"
+                      style={{
+                        width: "100%"
+                      }}
+                    >
+                      <ImageIcon />Upload Image
+                      <input
+                        type="file"
+                        accept="image/*"
+                        hidden
+                        onChange={handleFileUpload}
+                      />
+                    </Button>
+                  }
+                  { isUploaded &&
+                    <Button
+                      variant="contained"
+                      component="label"
+                      color="primary"
+                      style={{
+                        width: "100%"
+                      }}
+                    >
+                      <DoneIcon />Image Uploaded
+                      <input
+                        type="file"
+                        accept="image/*"
+                        hidden
+                        onChange={handleFileUpload}
+                      />
+                    </Button>
+                  }
+                </Grid>
+                <Grid item xs={5}>
+                  <Button
+                    type="submit"
+                    variant="contained"
+                    color="secondary"
+                    style={{
+                      width: "100%",
+                    }}
+                  >
+                    Confirm Edit
+                  </Button>
+                </Grid>
+              </Grid>
+            </Grid>
+          </form>
+        </Grid>
+      </Grid>
     
     </div>
   );
